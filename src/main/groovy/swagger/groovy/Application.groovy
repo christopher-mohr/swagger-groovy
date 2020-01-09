324faeb9b3180ea2456814c18a064f47c31cc0a3
Christopher Mohr <christopher.mohr@uni-tuebingen.de>
2020-01-09 10:12:54 +0100

    Initial commit

---
+++ src/main/groovy/swagger/groovy/Application.groovy
@@ -0,0 +1,19 @@
+package swagger.groovy
+
+import io.micronaut.runtime.Micronaut
+import groovy.transform.CompileStatic
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "swagger-groovy",
+            version = "0.0"
+    )
+)
+@CompileStatic
+class Application {
+    static void main(String[] args) {
+        Micronaut.run(Application)
+    }
+}
